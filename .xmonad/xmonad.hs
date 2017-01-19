7024c53b4b4c0e9fe64371f51877214a1702144d
Chris Stryczynski <chris@soundshare.co.uk>
2017-01-19 11:56:35 +0000

    Swopped keys - wrong direction

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -11,13 +11,13 @@ import qualified XMonad.StackSet as W
 
 import XMonad.Actions.CycleWS
 
-main = xmonad =<< xmobar desktopConfig
-    { borderWidth        = 10
+main = xmonad desktopConfig
+    { borderWidth        = 5
     , terminal           = "termite"
     , normalBorderColor  = "#cccccc"
     , focusedBorderColor = "#cd8b00"
     , modMask     = mod4Mask
-    , layoutHook         = avoidStruts $ layoutHook desktopConfig
+    , layoutHook         = desktopLayoutModifiers $ avoidStruts $ layoutHook desktopConfig
     , keys = myKeys <+> keys def
     , workspaces = ["1", "2", "3"]
     }
@@ -27,8 +27,8 @@ myKeys conf@XConfig {XMonad.modMask = modm} = M.fromList
             , ((modm, xK_e), windows W.focusUp)
             , ((modm .|. shiftMask, xK_u), windows W.swapDown)
             , ((modm .|. shiftMask, xK_e), windows W.swapUp)
-            , ((modm, xK_h), nextWS)
-            , ((modm, xK_t), prevWS)
+            , ((modm, xK_h), prevWS)
+            , ((modm, xK_t), nextWS)
             , ((modm, xK_l), withFocused $ windows . W.sink)
             , ((modm, xK_i), sendMessage Shrink)
             , ((modm, xK_d), sendMessage Expand)
