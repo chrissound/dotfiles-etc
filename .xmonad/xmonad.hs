527ff099191643972b65c8285f010b5a8765a9fb
Chris Stryczynski <chris@soundshare.co.uk>
2017-04-15 11:56:42 +0100

    woo

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,21 +1,22 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 import XMonad
 import XMonad.Config.Desktop
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-
-import XMonad.Hooks.Minimize
-import XMonad.Layout.Minimize
-
-
+import XMonad.Layout.ResizableTile
 import qualified Data.Map as M
-import XMonad.Prompt
 import qualified XMonad.StackSet as W
-
 import XMonad.Actions.CycleWS
 import XMonad.Actions.Navigation2D
-import XMonad.Layout.DragPane
 
-main = xmonad $ navigation2D def
+import XMonad.Layout.Spacing
+import MiddleColumn
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.ThreeColumns
+import XMonad.Actions.UpdatePointer
+
+main :: IO ()
+main = xmonad $ ewmh $ navigation2D def
   (xK_k, xK_h, xK_j, xK_l)
   [
     (mod4Mask,               windowGo  ),
@@ -23,30 +24,45 @@ main = xmonad $ navigation2D def
   ]
   True
   $ desktopConfig
-    { borderWidth        = 4
+    { borderWidth        = 3
     , terminal           = "lxterminal"
     , normalBorderColor  = "#cccccc"
     , focusedBorderColor = "#cd8b00"
     , modMask     = mod4Mask
     , keys = myKeys <+> keys def
-    , workspaces = ["a", "b", "c", "d"]
-    , layoutHook = desktopLayoutModifiers $ Tall 1 (5/100) (1/2) ||| Full
+    , workspaces = ["a", "b", "c", "d", "e"]
+    , layoutHook = desktopLayoutModifiers $
+      spacing 5 (MiddleColumn 0.25 1 0.040 0.25) |||
+      ThreeCol 1 (3/100) (1/2) |||
+      ResizableTall 2 (5/100) (1/2) [0.3, 0.5, 2] |||
+      tallLayout |||
+      Full
+    , handleEventHook = handleEventHook def <+> fullscreenEventHook
+    -- , logHook = dynamicLog
     }
 
+  where tallLayout = Tall 1 (5/100) (1/2)
+
+myKeys :: XConfig l -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@XConfig {XMonad.modMask = modm} = M.fromList
             [-- ((modm, xK_u), windows W.focusDown)
             --, ((modm, xK_e), windows W.focusUp)
             --, ((modm .|. shiftMask, xK_u), windows W.swapDown)
             --, ((modm .|. shiftMask, xK_e), windows W.swapUp)
-              ((modm, xK_e), prevWS)
+              ((modm, xK_backslash), windows W.focusDown)
+            , ((modm, xK_f), updatePointer (0.5, 0.5) (0, 0))
+            , ((modm, xK_e), prevWS)
             , ((modm, xK_u), nextWS)
             , ((modm, xK_l), withFocused $ windows . W.sink)
-            , ((modm .|. shiftMask, xK_i), sendMessage Shrink)
-            , ((modm .|. shiftMask, xK_d), sendMessage Expand)
-            , ((modm, xK_p), spawn "rofi -combi-modi drun,window -show combi -modi combi")
-            , ((modm .|. shiftMask, xK_z), devSession)
+            , ((modm, xK_i), sendMessage Shrink)
+            , ((modm, xK_d), sendMessage Expand)
+            , ((modm .|. shiftMask, xK_i), sendMessage MirrorShrink)
+            , ((modm .|. shiftMask, xK_d), sendMessage MirrorExpand)
+            , ((modm, xK_p), spawn "rofi  -combi-modi drun,window -show combi -modi combi")
+            , ((modm .|. shiftMask, xK_z), devSessionPrompt)
             , ((modm, xK_slash), spawn $ XMonad.terminal conf)
+            --, ((modm, (fromIntegral button3)), (\w -> focus w >> Flex.mouseResizeWindow w))
             ]
 
-devSession :: X ()
-devSession = spawn " ~/Scripts/devEnv.sh ~/Scripts/openTerminal.sh"
+devSessionPrompt :: X ()
+devSessionPrompt = spawn "rofi -normal-window -show fb -modi fb:~/Scripts/rofi/xmonadRofi.sh"
